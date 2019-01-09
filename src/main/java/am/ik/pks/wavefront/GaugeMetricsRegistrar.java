32b408c81c9a00896b66e6258aa23e81b15097b3
Toshiaki Maki <tmaki@pivotal.io>
2019-01-09 21:40:29 +0900

    Increase cache duration

--- src/main/java/am/ik/pks/wavefront/GaugeMetricsRegistrar.java
+++ src/main/java/am/ik/pks/wavefront/GaugeMetricsRegistrar.java
@@ -1,7 +1,7 @@
 package am.ik.pks.wavefront;
 
+import java.time.Duration;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
@@ -18,33 +18,35 @@ import org.springframework.stereotype.Component;
 @Component
 public class GaugeMetricsRegistrar {
 
-    private static final Logger log = LoggerFactory
-        .getLogger(GaugeMetricsRegistrar.class);
+	private static final Logger log = LoggerFactory
+			.getLogger(GaugeMetricsRegistrar.class);
 
-    private final Cache<Tuple2<String, List<Tag>>, GaugeMetrics> cache;
+	private final Cache<Tuple2<String, List<Tag>>, GaugeMetrics> cache;
 
-    private final MeterRegistry meterRegistry;
+	private final MeterRegistry meterRegistry;
 
-    public GaugeMetricsRegistrar(MeterRegistry meterRegistry) {
-        this.meterRegistry = meterRegistry;
-        Cache<Tuple2<String, List<Tag>>, GaugeMetrics> caffeine = Caffeine.newBuilder() //
-            .expireAfterWrite(2, TimeUnit.MINUTES) //
-            .build();
-        this.cache = CaffeineCacheMetrics.monitor(meterRegistry, caffeine, "gaugeMetrics");
-    }
+	public GaugeMetricsRegistrar(MeterRegistry meterRegistry) {
+		this.meterRegistry = meterRegistry;
+		Cache<Tuple2<String, List<Tag>>, GaugeMetrics> caffeine = Caffeine.newBuilder() //
+				.expireAfterWrite(Duration.ofMinutes(10)) //
+				.build();
+		this.cache = CaffeineCacheMetrics.monitor(meterRegistry, caffeine,
+				"gaugeMetrics");
+	}
 
-    public void register(GaugeMetrics metrics) {
-        try {
-            Tuple2<String, List<Tag>> key = metrics.key();
-            cache.put(key, metrics);
-            Gauge.builder(metrics.name(), cache, value -> {
-                GaugeMetrics gaugeMetrics = value.get(key, x -> null);
-                return gaugeMetrics == null ? Double.NaN : gaugeMetrics.value();
-            }) //
-                .tags(metrics.tags()) //
-                .register(this.meterRegistry);
-        } catch (IllegalArgumentException e) {
-            log.debug("{}", e.getMessage());
-        }
-    }
+	public void register(GaugeMetrics metrics) {
+		try {
+			Tuple2<String, List<Tag>> key = metrics.key();
+			cache.put(key, metrics);
+			Gauge.builder(metrics.name(), cache, value -> {
+				GaugeMetrics gaugeMetrics = value.get(key, x -> null);
+				return gaugeMetrics == null ? Double.NaN : gaugeMetrics.value();
+			}) //
+					.tags(metrics.tags()) //
+					.register(this.meterRegistry);
+		}
+		catch (IllegalArgumentException e) {
+			log.debug("{}", e.getMessage());
+		}
+	}
 }
